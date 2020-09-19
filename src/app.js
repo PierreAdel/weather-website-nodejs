3efaf4576dbc00212a9e6dbc4eff90a99dba365e
Pierre Adel <pierreadelkamel@gmail.com>
2020-09-19 17:13:52 +0200

    sending html and JSON

--- src/app.js
+++ src/app.js
@@ -5,22 +5,30 @@ const app = express()
 
 app.get('', (req,res) =>
 {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help',(req,res)=>
 {
-    res.send('How can we help you?')
+    res.send({
+        name: 'Pierre',
+        age:21
+
+    })
+})
+
+
+app.get('/about',(req,res)=>
+{
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather',(req,res)=>
 {
-    res.send('Weather page')
-})
-
-app.get('/about',(req,res)=>
-{
-    res.send('About page')
+    res.send({
+        temp: 12,
+        precip: '0%'
+    })
 })
 
 app.listen(3000, () =>
