1b8b195e682c7f8dee63a449190e4a57c839f58d
Pierre Adel <pierreadelkamel@gmail.com>
2020-09-20 19:52:12 +0200

    end-of-section-7

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 const path = require('path')
 
 const express = require('express')
+const hbs = require('hbs')
 
 console.log(__dirname)
 console.log(path.join(__dirname,'../public'))
 
 const app = express()
 
+//define paths for express config
 const pubDirPath = path.join(__dirname,'../public')
+const viewPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 
+// setup handlebars engine and views location
+app.set('views',viewPath)
 app.set('view engine', 'hbs')
+hbs.registerPartials(partialsPath)
 
+// setup static directory to serve
 app.use(express.static(pubDirPath))
+
 app.get('',(req,res) =>
 {
     res.render('index', {
@@ -35,15 +44,21 @@ app.get('/about',(req,res) =>
 app.get('/help',(req,res) =>
 {
     res.render('help', {
-
+        helptext: 'this is some helpful text',
         title:'help',
         name: 'Pierre'
     })
 
 
 })
+app.get('/help*',(req,res)=>
+{
+    res.render('404page', {
+        title:'404 Help article not found',
+        name: 'Pierre'
 
-
+   })
+})
 
 app.get('/weather',(req,res)=>
 {
@@ -51,6 +66,21 @@ app.get('/weather',(req,res)=>
         temp: 12,
         precip: '0%'
     })
+})
+
+app.get('*',(req,res)=>
+{
+   
+   res.render('404page', {
+        title:'404 page not found!',
+        name: 'Pierre'
+
+   })
+})
+
+app.get('*',(req,res)=>
+{
+   res.send('404 page not found!')
 })
 
 app.listen(3000, () =>
